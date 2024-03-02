0d6ce41e2cb2c276587ee95019706425a5075c9b
Devajit <patar1447@gmail.com>
2024-03-03 00:19:54 +0530

    Create a QuizGenerate activity and Intent from HomeFragment

---
+++ app/src/main/java/com/compose/android/dev/edu/devpost/happyhacksv/digiacademy/ui/activity/QuizGenerateActivity.kt
@@ -0,0 +1,12 @@
+package com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.R
+
+class QuizGenerateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_generate)
+    }
+}
--- app/src/main/java/com/compose/android/dev/edu/devpost/happyhacksv/digiacademy/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/compose/android/dev/edu/devpost/happyhacksv/digiacademy/ui/fragment/HomeFragment.kt
@@ -1,16 +1,22 @@
 package com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.R
+import com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.databinding.FragmentHomeBinding
+import com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.ui.activity.QuizGenerateActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +30,15 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnTakeQuiz.setOnClickListener {
+            startActivity(Intent(requireContext(), QuizGenerateActivity::class.java))
+        }    }
 
 }
