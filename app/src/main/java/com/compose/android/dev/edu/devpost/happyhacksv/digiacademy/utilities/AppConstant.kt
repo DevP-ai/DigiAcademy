e9a2f1f790022324772f3fc8876182bb98e8d40d
Devajit <patar1447@gmail.com>
2024-03-02 16:20:15 +0530

    News and Quiz API Base URl added

---
+++ app/src/main/java/com/compose/android/dev/edu/devpost/happyhacksv/digiacademy/utilities/AppConstant.kt
@@ -0,0 +1,8 @@
+package com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.utilities
+
+object AppConstant {
+    const val NEWS_API_BASE_URL="https://newsapi.org/v2/"
+    const val NEWS_API_KEY="782b4508b81a4eab9cf6f69934202d86"
+
+    const val QUIZ_API_BASE_URL="https://opentdb.com"
+}
