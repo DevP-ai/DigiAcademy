10056ad67ac5dd298b1cf2955ddd803daec187c3
Devajit <patar1447@gmail.com>
2024-03-02 18:08:49 +0530

    SpalshScreen Implemented

---
+++ app/src/main/java/com/compose/android/dev/edu/devpost/happyhacksv/digiacademy/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.MainActivity
+import com.compose.android.dev.edu.devpost.happyhacksv.digiacademy.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2000)
+
+    }
+}
